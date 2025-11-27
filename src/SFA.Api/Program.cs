8a08d98a71fc726c98943dd5fe55ca2d3420eb65
Matheus Vinicius <devmatheusoxs@gmail.com>
2025-11-26 22:37:39 -0300

    feat: voltando o serilog pro program.cs e subindo as novas migrations pra teste no deploy

--- src/SFA.Api/Program.cs
+++ src/SFA.Api/Program.cs
@@ -18,9 +18,9 @@ using Amazon.Runtime.Credentials;
 var builder = WebApplication.CreateBuilder(args);
 
 // Serilog básico
-// builder.Host.UseSerilog((ctx, lc) => lc
-//     .ReadFrom.Configuration(ctx.Configuration)
-//     .WriteTo.Console());
+builder.Host.UseSerilog((ctx, lc) => lc
+    .ReadFrom.Configuration(ctx.Configuration)
+    .WriteTo.Console());
 
 string BuildConnectionString(IConfiguration cfg)
 {
